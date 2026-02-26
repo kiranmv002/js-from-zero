e1135ace9a2593ddd7b168e3b3020bfa8d33e24d
M V Kiran <mada.kiran@s.amity.edu>
2026-02-26 21:58:47 +0530

    Update functions.js

--- 01-basics/functions.js
+++ 01-basics/functions.js
@@ -1,1 +1,31 @@
+// ==============================
+// Day 2 - Functions in JavaScript
+// ==============================
+
+
+// ----------------------------
+// Function Declaration
+// the classic way to write a function
+// can be called even before it is defined (hoisting)
+// ----------------------------
+
+function greet(name) {
+    return `hey ${name}, welcome!`
+}
+
+console.log(greet("Kiran"))   // hey Kiran, welcome!
+console.log(greet("Ravi"))    // hey Ravi, welcome!
+
+
+// ----------------------------
+// Function with multiple parameters
+// ----------------------------
+
+function add(a, b) {
+    return a + b
+}
+
+console.log(add(5, 10))    // 15
+console.log(add(20, 30))   // 50
+
 
