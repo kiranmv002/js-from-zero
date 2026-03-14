d8625d82a74fca388831d5cf419f2223a92a5128
M V Kiran <mada.kiran@s.amity.edu>
2026-03-14 20:25:50 +0530

    Create  objects-basics.js

---
+++ 01-basics/objects-basics.js
@@ -0,0 +1,21 @@
+// Day 6 - Objects in JavaScript
+
+// creating an object
+// objects store related data as key value pairs
+const student = {
+    name: "Kiran",
+    age: 19,
+    city: "Hyderabad",
+    isStudent: true
+}
+
+console.log(student)
+
+
+// accessing properties
+// dot notation
+console.log(student.name)    // Kiran
+console.log(student.age)     // 19
+
+// bracket notation
+console.log(student["city"])   // Hyderabad
