352f53df7b5b5c5f31cf6efe303d042ba848cbd4
M V Kiran <mada.kiran@s.amity.edu>
2026-03-14 20:30:34 +0530

    Update objects-basics.js

--- 01-basics/objects-basics.js
+++ 01-basics/objects-basics.js
@@ -19,3 +19,38 @@ console.log(student.age)     // 19
 
 // bracket notation
 console.log(student["city"])   // Hyderabad
+
+
+// adding new property
+student.college = "JNTU"
+console.log(student.college)   // JNTU
+
+
+// updating a property
+student.age = 20
+console.log(student.age)   // 20
+
+
+// deleting a property
+delete student.isStudent
+console.log(student)
+
+
+// checking if property exists
+console.log("name" in student)    // true
+console.log("phone" in student)   // false
+
+
+// --- object methods ---
+
+// Object.keys - get all keys
+const keys = Object.keys(student)
+console.log(keys)   // [name, age, city, college]
+
+// Object.values - get all values
+const values = Object.values(student)
+console.log(values)   // [Kiran, 20, Hyderabad, JNTU]
+
+// Object.entries - get key value pairs
+const entries = Object.entries(student)
+console.log(entries)
