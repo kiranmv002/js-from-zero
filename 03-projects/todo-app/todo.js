0b123678b4b619ed76e5131731da610f6a04d07b
M V Kiran <mada.kiran@s.amity.edu>
2026-04-11 23:20:56 +0530

    Update todo.js

--- 03-projects/todo-app/todo.js
+++ 03-projects/todo-app/todo.js
@@ -1,3 +1,13 @@
 // Day 10 - Mini Project: Todo App
 // uses everything learned so far:
 // arrays, objects, functions, dom, events, local storage
+
+// --- state ---
+let todos = JSON.parse(localStorage.getItem('todos')) || []
+let currentFilter = 'all'
+
+
+// --- save to local storage ---
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
